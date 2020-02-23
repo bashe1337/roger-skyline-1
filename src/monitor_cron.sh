1f66a4610cb27b66fe18fbf5bd7e8f76eaba4f17
Bess Ashe <bashe@oa-a4.21-school.ru>
2020-02-23 19:43:05 +0300

    monitor

---
+++ src/monitor_cron.sh
@@ -0,0 +1,16 @@
+#!bin/sh
+MSG="/etc/crontab has been modified!"
+SUBJECT="Monitor changes crontab"
+ROOT="root@debian.lan"
+HASH="/etc/cron.d/hash"
+FILE="/etc/crontab"
+CH_FILE="/etc/cron.d/cron_hash_md5"
+test -f $HASH || sudo touch $HASH
+CRON_HASH=$(sudo md5sum $FILE)
+sudo touch $CH_FILE
+sudo echo $CRON_HASH > $CH_FILE
+if [ "$(cat $HASH)" != "$(cat $CH_FILE)" ]; then
+	echo $CRON_HASH > $HASH
+	echo $MSG | mail -s "$SUBJECT" $ROOT
+fi;
+exit
